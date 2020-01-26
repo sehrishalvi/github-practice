58808bda3cce1bc21e5fcfb02cc847cdd9e375c7
sehrishalvi <55538190+sehrishalvi@users.noreply.github.com>
2020-01-26 11:52:48 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# github-practice
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello github</h1>
+      <h2>connecting github !</h2>
+    </div>
+  );
+}
